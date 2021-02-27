79f587e722bd7d0d9fd6bdb389b2c66543ade689
xiaolaba <xiao_laba_cn@yahoo.com>
2021-02-27 15:19:28 +0800

    Add files via upload

--- flash_light.h
+++ flash_light.h
@@ -3,20 +3,19 @@
  * 2020-JUL-01
  */
 
-//#define NodeMCU-32S 1
-#define ESP32-CAM 1
+#define NodeMCU_ESP-32S 1 // 2021-FEB-27, metal can printed ESP-WROOM-32, no PSRAM installed
+//#define ESP32_CAM 1
 
 //if you do not need LED_FLASH lights up image during capture, comment out this line
 //#define USE_FLASH_LIGHT 1   // used at app_httpd.cpp 
 
-
-#ifdef NodeMCU-32S //2017-07-17 ESP32 testing, NodeMCU-32S broad, xiaolaba
+#ifdef NodeMCU_ESP-32S //2017-07-17 ESP32 testing, NodeMCU-32S broad, xiaolaba
   #define LED1 2        //GPIO-2 
   #define LED_FLASH1 2  //dummy, no FLASH_LED, for easy coding only as included ESP32-CAM for testing
 #endif
 
 
-#ifdef ESP32-CAM //2020-07-01, ESP32-CAM testing, xiaolaba
+#ifdef ESP32_CAM //2020-07-01, ESP32-CAM testing, xiaolaba
 #define LED_FLASH1  4   // ESP32-CAM LED flash light, GPIO-4
 #define LED1        33  // ESP32-CAM on-broad tiny LED, GPIO-33
 #endif
